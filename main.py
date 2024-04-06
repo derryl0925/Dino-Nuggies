8263ef00df1f4ddc2ed2a076c3a8144613b9ccdb
Derrick Lin <derrick_lin@comcast.net>
2024-04-06 16:38:29 -0700

    Update main.py
    cleaned dino

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-import tensorflow as tf
+import babypandas as bpd
+import numpy as np
 
-from tensorflow.python.client import device_lib
+import matplotlib.pyplot as plt
+plt.style.use('ggplot')
 
+
+# Load the CSV file into a DataFrame
+df = pd.read_csv('DinoNuggieFindings.csv')
+
+# Cleaning Dinosaur data, columns to be removed
+columns_to_remove = ["occurrence_no", "record_type", "reid_no", "collection_no", "accepted_attr", "accepted_no", "max_ma", "min_ma", "reference_no", "time_bins", "time_contain", "time_major", "time_buffer", "time_overlap"]
+
+# Remove the specified columns
+df = df.drop(columns_to_remove, axis=1)
+
+# Save the modified DataFrame back to a CSV file
+df.to_csv('DinoNuggieFindings_modified.csv', index=False)
