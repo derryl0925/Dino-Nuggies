af1b1bf1971b38133e44bcc527e2533f155c7862
ethanmypan <eeperez@ucsd.edu>
2024-04-06 19:39:07 -0700

    title
    updated data so its easy to interepres

--- main.py
+++ main.py
@@ -7,16 +7,15 @@ plt.style.use('ggplot')
 
 # Cleaning Dinosaur data
 # Load the CSV file into a DataFrame
-df = pd.read_csv('DinoNuggieFindings.csv')
+#df = pd.read_csv('dinoData.csv')
+df = pd.read_csv('dinoData.csv', usecols=['cc'])
 
-# Columns to be removed
-columns_to_remove = ["occurrence_no", "record_type", "reid_no", "collection_no", "accepted_attr", "accepted_no", "max_ma", "min_ma", "reference_no", "time_bins", "time_contain", "time_major", "time_buffer", "time_overlap"]
-
-# Remove the specified columns
-df = df.drop(columns_to_remove, axis=1)
 
 # Save the modified DataFrame back to a CSV file
-df.to_csv('DinoNuggieFindings_modified.csv', index=False)
+df.to_csv('dinoDataClean.csv', index=False)
+count_df = df.groupby('cc').size().reset_index(name='count')
+count_df = count_df.sort_values(by='count', ascending=False)
+print(count_df)
 
 # Cleaning fossil fuel data
 # Load the CSV file into a DataFrame
@@ -40,8 +39,33 @@ columns_to_remove = [
 # Remove the specified columns
 df = df.drop(columns_to_remove, axis=1)
 
+grouped_df = df.groupby('Entity').sum()
+grouped_df = grouped_df.drop(index='World', errors='ignore')
+
+print(grouped_df)
+
 # Save the modified DataFrame back to a CSV file
 df.to_csv('Fuel_production_vs_consumption_modified.csv', index=False)
+
+sorted_oil_df = grouped_df.sort_values(by='Oil production(m³)', ascending=False)
+
+# Sort by most gas production
+sorted_gas_df = grouped_df.sort_values(by='Gas production(m³)', ascending=False)
+
+# Sort by most coal production
+sorted_coal_df = grouped_df.sort_values(by='Coal production(Ton)', ascending=False)
+
+print("Most oil production:")
+print(sorted_oil_df)
+
+print("\nMost gas production:")
+print(sorted_gas_df)
+
+print("\nMost coal production:")
+print(sorted_coal_df)
+
+
+
 
 
 '''
